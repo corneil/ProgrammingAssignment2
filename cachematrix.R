739f751d476a908b3c6900b5b2aa286cea89882f
Corneil du Plessis corneil.duplessis@gmail.com <Corneil du Plessis corneil.duplessis@gmail.com>
2014-12-19 13:42:51 +0200

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions that will create an object that can be used to cache the inverse of the matrix.
 
-## Write a short comment describing this function
-
+## Create an object with a methods and place holder for holding the solved inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        ## If x is modified set i to NULL
+        x <<- y
+        i <<- NULL        
+    }
+    get <- function() x
+    setinverse <- function(solve) i <<- solve
+    getinverse <- function() i
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Return the place holder if not NULL other solve the inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if(!is.null(i)) {
+            return(i)
+        }
+        data <- x$get()
+        print(class(data))
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
 }
